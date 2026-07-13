5a972dde14c4024aa315204d23495cbe72a7a39e
SNLRLONG <seavlongvann55@gmail.com>
2026-07-13 18:10:11 +0700

    server ts

--- backendSheetDB/server.ts
+++ backendSheetDB/server.ts
@@ -2,6 +2,8 @@ import { env } from './config/env'
 import { createAdapter } from './config/adapter'
 import { createApp } from './app'
 
+// Entry point: wires the adapter (Sheets or Postgres, per DB_DRIVER) into the Express app.
+
 const { adapter, storage } = createAdapter()
 const app = createApp(adapter, storage)
 
